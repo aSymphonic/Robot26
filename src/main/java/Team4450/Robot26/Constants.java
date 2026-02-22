0ef9ee8163d5b5843accca3669a4d5f78d47092f
anon <anon@email.com>
2026-02-22 15:02:37 -0800

    Constants stuff

--- src/main/java/Team4450/Robot26/Constants.java
+++ src/main/java/Team4450/Robot26/Constants.java
@@ -75,10 +75,10 @@ public final class Constants {
     public static int INTAKE_CURRENT_LIMIT = 20;
     public static int HOPPER_CURRENT_LIMIT = 40;
 
-    // public static double HOOD_TOLERENCE_MOTOR_ROTATIONS = 0.02;
-    // public static double HOOD_TOLERENCE_DEGREES = 5;
-    // public static double HOOD_GEAR_RATIO = 3/8; //Make sure to change this constant in Shooter class for the hood angle calculation
-    // public static double HOOD_DOWN_ANGLE_DEGREES = 15;
+    public static double HOOD_TOLRENCE_MOTOR_ROTATIONS = 0.02;
+    public static double HOOD_TOLERENCE_DEGREES = 5;
+    public static double HOOD_GEAR_RATIO = 3/8; //Make sure to change this constant in Shooter class for the hood angle calculation
+    public static double HOOD_DOWN_ANGLE_DEGREES = 15;
 
     public static double SHOOTER_HOOD_TOLERENCE_MOTOR_RAIDIANS = 2;
     
