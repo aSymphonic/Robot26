93cf14e1c9f8bea3da022f8a644df4b3252f4d02
colepearson27 <colepearson27@gmail.com>
2026-03-18 19:27:26 -0700

    Removed compass from hub tracking.

--- src/main/java/Team4450/Robot26/commands/DriveCommand.java
+++ src/main/java/Team4450/Robot26/commands/DriveCommand.java
@@ -100,7 +100,8 @@ public class DriveCommand extends Command {
             }
             // If there IS input, set the target heading to where the joystick is facing in relation to the driver
         } else {
-            targetHeading = -Math.toDegrees(Math.atan2(rotationYSupplier.getAsDouble(), rotationXSupplier.getAsDouble())) - 90;
+            // targetHeading = -Math.toDegrees(Math.atan2(rotationYSupplier.getAsDouble(), rotationXSupplier.getAsDouble())) - 90;
+            targetHeading = drivebase.getAngleToAim(hubPosition);
         }
 
         targetHeading = normalizeAngle(targetHeading);
